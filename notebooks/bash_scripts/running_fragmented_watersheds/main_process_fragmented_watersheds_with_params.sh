a33086aef99fab16fbc3b76db67c775ba8d67c70
Luc Rébillout <rebillout.luc@gmail.com>
2025-01-07 17:32:42 -0600

    better decrement of num_jobs

--- notebooks/bash_scripts/running_fragmented_watersheds/main_process_fragmented_watersheds_with_params.sh
+++ notebooks/bash_scripts/running_fragmented_watersheds/main_process_fragmented_watersheds_with_params.sh
@@ -133,7 +133,7 @@ num_jobs=0
 if [ -f "${ROOT_DIR}/thuc_list_to_run_params.csv" ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Reusing existing thuc_list_to_run_params.csv file" | tee -a "$LOG_FILE"
     num_jobs=$(wc -l < "${ROOT_DIR}/thuc_list_to_run_params.csv")
-    num_jobs-=1 # Account for the header
+    num_jobs=$(($num_jobs-1)) # Account for the header
 fi
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Found $num_jobs watersheds" | tee -a "$LOG_FILE"
